54199bb7b9e638d87c0d31b4737dca80d9c85075
Your Name <you@example.com>
2021-04-12 19:59:49 +0300

    auto

--- ci/env/utils.py
+++ ci/env/utils.py
@@ -58,7 +58,11 @@ def django_conf(env_id):
     sname = '%s.django.%s' % (env.name, settings.DOMAIN)
     tpl = tpl.replace('%name%', sname)
     tpl = tpl.replace('%port%', str(envp.port))
-    prj_dir = os.path.join(settings.WORK_DIR, env.name, env.project.venv_path)
+    if env.project.venv_path:
+        prj_dir = os.path.join(settings.WORK_DIR, env.name, env.project.venv_path)
+    else:
+        prj_dir = os.path.join(settings.WORK_DIR, env.name)
+    
     tpl = tpl.replace('%prj_dir%', prj_dir)
     tpl = tpl.replace('%ci_dir%', str(settings.BASE_DIR))
     tpl = tpl.replace('%user%', settings.USER)
