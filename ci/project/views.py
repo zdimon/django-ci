1a2e482557099dad15be2d5c2edc4ba7a11d98af
Your Name <you@example.com>
2021-04-12 15:19:49 +0300

    auto

--- ci/project/views.py
+++ ci/project/views.py
@@ -4,6 +4,16 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from env.models import Environ
+from django.shortcuts import redirect
+
+
+@login_required
+def pre_remove(request,id):
+    projects = Project.objects.get(pk=id)
+    env = Environ.objects.filter(user=request.user, project=project)
+    return redirect(f'/env/pre_remove/{env.id}')
+
+
 
 
 @login_required
