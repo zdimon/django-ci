a595a0d871a9d45895f40bcf0e23dc1681b7c92e
Your Name <you@example.com>
2021-04-12 15:13:28 +0300

    auto

--- ci/project/views.py
+++ ci/project/views.py
@@ -3,9 +3,13 @@ from .models import Project
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from env.models import Environ
 
 
 @login_required
 def list(request):
     projects = Project.objects.all()
-    return render(request, 'project/list.html', {"projects": projects})
+    eid = []
+    for e in Environ.objects.filter(user=request.user):
+        eid.append(e.project.id)
+    return render(request, 'project/list.html', {"projects": projects, "eid": eid})
