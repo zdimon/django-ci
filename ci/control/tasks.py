801a2f8513a0968e863d65a50a92d18ae2b25553
Your Name <you@example.com>
2021-04-14 22:46:18 +0300

    auto

--- ci/control/tasks.py
+++ ci/control/tasks.py
@@ -23,19 +23,19 @@ def git_commit(env_id):
     path = os.path.join(settings.WORK_DIR, env.name)
     repo = Repo(path)
     t = repo.head.commit.tree
-    if repo.git.diff(t):
-        print('Make commit')
-        repo.git.add(update=True)
-        comment = 'commit from %s project %s' % (env.user.username, env.project.name)
-        r = repo.index.commit(comment)
-        env.state = 'edited'
-        env.save()
-        save_commit(comment, env.user)
-        os.chdir(path)
-        out = run_command('git push')
-        return {"error": None, "output": f'Данные закомичены. {out["output"]} '}
-    else:
-        return {"error": None, "output": 'Нечего комитить.'}
+    #if repo.git.diff(t):
+    print('Make commit')
+    repo.git.add(update=True)
+    comment = 'commit from %s project %s' % (env.user.username, env.project.name)
+    r = repo.index.commit(comment)
+    env.state = 'edited'
+    env.save()
+    save_commit(comment, env.user)
+    os.chdir(path)
+    out = run_command('git push')
+    return {"error": None, "output": f'Данные закомичены. {out["output"]} '}
+    # else:
+    #     return {"error": None, "output": 'Нечего комитить.'}
 
     #print(r)
     # os.chdir(path)
