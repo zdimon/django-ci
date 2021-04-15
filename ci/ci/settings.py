ac7aca2a5c6eaada505d7455354e0e30ee0321e4
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-15 15:14:04 +0300

    auto

--- ci/ci/settings.py
+++ ci/ci/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
