99362c2e63dea2fb5446a824ab6ca5ac04c0b86f
Your Name <you@example.com>
2021-04-20 12:12:13 +0300

    auto

--- ci/main/translation.py
+++ ci/main/translation.py
@@ -1,7 +1,12 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Page
+from .models import Page, Task
 
 class PageTranslationOptions(TranslationOptions):
     fields = ('title', 'content')
 
 translator.register(Page, PageTranslationOptions)
+
+class TaskTranslationOptions(TranslationOptions):
+    fields = ('title', 'desc')
+
+translator.register(Task, TaskTranslationOptions)
