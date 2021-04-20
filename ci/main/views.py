4b3884b0a37ceaa3f7efdfb69dd2ced1f6e320dc
Your Name <you@example.com>
2021-04-20 10:48:55 +0300

    auto

--- ci/main/views.py
+++ ci/main/views.py
@@ -21,7 +21,7 @@ from django.utils.translation import check_for_language
 
 
 def set_language(request):
-    next = request.REQUEST.get('next', None)
+    next = request.GET.get('next', None)
     if not next:
         next = request.META.get('HTTP_REFERER', None)
     if not next:
@@ -29,12 +29,12 @@ def set_language(request):
     response = HttpResponseRedirect(next)
     if request.method == 'GET':
         lang_code = request.GET.get('language', None)
-        if lang_code and check_for_language(lang_code):
-            if hasattr(request, 'session'):
-                request.session['django_language'] = lang_code
-            else:
-                response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
-            translation.activate(lang_code)
+        #if lang_code and check_for_language(lang_code):
+        if hasattr(request, 'session'):
+            request.session['django_language'] = lang_code
+        else:
+            response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
+        translation.activate(lang_code)
     return response
 
 
